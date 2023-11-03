120d5cc95de74b7c5fee72093bfe33543b94aa55
blazej-kosmowski <blazej.kosmowski@legimi.com>
2023-11-03 14:45:08 +0100

    Added methods for detecting kindle, and managing files with small fixes

--- src/LibMtpSharpStandardMacOS/OpenedMtpDevice.cs
+++ src/LibMtpSharpStandardMacOS/OpenedMtpDevice.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using LibMtpSharpStandardMacOS.Enums;
 using LibMtpSharpStandardMacOS.Exceptions;
@@ -13,6 +14,7 @@ namespace LibMtpSharpStandardMacOS
 {
     public class OpenedMtpDevice : IDisposable
     {
+        private const int KindleVendorId = 0x1949;
         private readonly IntPtr _mptDeviceStructPointer;
         private readonly bool _cached;
         private readonly uint _vendorId;
@@ -29,6 +31,8 @@ namespace LibMtpSharpStandardMacOS
         }
 
         public uint GetVendorId() => _vendorId;
+
+        public bool IsKindle() => _vendorId == KindleVendorId;
 
         private MtpDeviceStruct CurrentMtpDeviceStruct =>
             Marshal.PtrToStructure<MtpDeviceStruct>(_mptDeviceStructPointer);
@@ -78,6 +82,10 @@ namespace LibMtpSharpStandardMacOS
             return result;
         }
 
+        public IEnumerable<FileStruct> EnumerateFiles(uint storageId,
+            uint parentId = LibMtpLibrary.LibmtpFilesAndFoldersRoot) =>
+            GetFolderContent(storageId, parentId).Where(x => x.Filetype != FileTypeEnum.Folder);
+
         public Option<FileStruct> GetFile(uint storageId, string fileName, string fileExtension) =>
             GetMatchingItem(storageId, x =>
             {
@@ -92,9 +100,9 @@ namespace LibMtpSharpStandardMacOS
                 return string.Compare(fileNameWithoutExtension, fileName, StringComparison.InvariantCultureIgnoreCase) == 0;
             });
 
-        public Option<FileStruct> GetDirectory(uint storageId, string fileName) =>
+        public Option<FileStruct> GetDirectory(uint storageId, string directoryName) =>
             GetMatchingItem(storageId, x =>
-                string.Compare(x.FileName, fileName, StringComparison.InvariantCultureIgnoreCase) == 0
+                string.Compare(x.FileName, directoryName, StringComparison.InvariantCultureIgnoreCase) == 0
                 && x.Filetype == FileTypeEnum.Folder);
 
         private Option<FileStruct> GetMatchingItem(uint storageId, Func<FileStruct, bool> predicate, uint parentId = LibMtpLibrary.LibmtpFilesAndFoldersRoot)
@@ -117,12 +125,12 @@ namespace LibMtpSharpStandardMacOS
             return Option.None<FileStruct>();
         }
 
-        public void CopyAzwFileToDevice(string filePath, uint storageId, uint parentId)
+        public void CopyFileToDevice(string filePath, string destFileName, uint storageId, uint parentId = LibMtpLibrary.LibmtpFilesAndFoldersRoot)
         {
             var fileInfo = new FileInfo(filePath);
             var fileStruct = new FileStruct
             {
-                FileName = fileInfo.Name,
+                FileName = destFileName,
                 FileSize = (ulong)fileInfo.Length,
                 Filetype = FileTypeEnum.Unknown,
                 ParentId = parentId,
@@ -132,6 +140,15 @@ namespace LibMtpSharpStandardMacOS
             var result = LibMtpLibrary.SendFile(_mptDeviceStructPointer, filePath, ref fileStruct, null, IntPtr.Zero);
             if (result != 0)
                 throw new CopyFileToDeviceException($"Cannot copy {fileInfo.Name} to device");
+        }
+
+        public void CopyFileFromDevice(uint fileId, string destinationPath)
+        {
+            if (string.IsNullOrEmpty(destinationPath))
+                throw new ArgumentException("Value cannot be null or empty.", nameof(destinationPath));
+
+            if (LibMtpLibrary.GetFileToFile(_mptDeviceStructPointer, fileId, destinationPath, null) != 0)
+                throw new CopyFileFromDeviceException(fileId, destinationPath);
         }
 
         public void Dispose()
@@ -145,7 +162,7 @@ namespace LibMtpSharpStandardMacOS
             ReleaseUnmanagedResources();
         }
 
-        public uint CreateFolder(string name, uint parentFolderId, uint parentStorageId)
+        public uint CreateFolder(string name, uint parentStorageId, uint parentFolderId = LibMtpLibrary.LibmtpFilesAndFoldersRoot)
         {
             var newFolderId =
                 LibMtpLibrary.CreateFolder(_mptDeviceStructPointer, name, parentFolderId, parentStorageId);
